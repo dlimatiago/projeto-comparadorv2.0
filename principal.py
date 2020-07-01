69f3633e802deb25eb0fe48e0b83817bd02abd0b
Tiago Lima <tiaguinho.alima@gmail.com>
2020-07-01 16:31:50 -0300

    Nome das importações atualizados, controle de atualização adicionado no código principal e output da cotação diária adicionado
    Signed-off-by: Tiago Lima <tiaguinho.alima@gmail.com>

--- principal.py
+++ principal.py
@@ -1,2 +1,15 @@
-import getdata as imp
+import pegadados as p
+import moedas as m
 
+h_atual = p.updates(True)
+tempo = p.delta(h_atual)
+
+if tempo > 12:  # Faz a atualização dos dados, se necessária
+    p.getcoins()
+
+usd_real, usd_guarani, usd_peso = m.usd_cot(), m.usd_cot('Paraguai'), m.usd_cot('Argentina')
+
+print(f'    Cotação do dia\n'
+      f'{usd_real}\n'
+      f'{usd_guarani}\n'
+      f'{usd_peso}')
